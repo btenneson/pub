28714c5c4df0dc725423f58af31be6a9bfd3de38
btenneson <btenneson2301@baypath.edu>
2026-09-02 02:11:27 -0700

    Publish expanded AMLD Security 3 companion manuscript

--- scripts/build_library_homepage_v3.py
+++ scripts/build_library_homepage_v3.py
@@ -43,6 +43,7 @@ RANKED_HREFS = (
     "papers/data_atp_creativity_treatise_v0_1/",
     "papers/what_checks_the_proof_v6_80/",
     "papers/amld_security_treatise_v0_1/",
+    "papers/amld_security_3/",
     "papers/verified_settlement_search_v1_4/",
     "papers/shortest_settlement_optimal_control_v1_0/",
     "papers/search_dynamics_abel_v0_4/",
